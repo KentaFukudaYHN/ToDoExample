9332f9ef1cec5c797f9d98ea58ad616a6a7328b1
KentaFukudaYHN <yhn18782@gmail.com>
2020-03-10 17:05:29 +0900

    一覧取得のテストを実装

--- ToDoExampleTest/Services/ToDoServiceTest.cs
+++ ToDoExampleTest/Services/ToDoServiceTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Moq;
 using ToDoExample.Entities;
 using ToDoExample.Interfaces;
@@ -12,6 +13,31 @@ namespace ToDoExampleTest.Services
     /// </summary>
     public class ToDoServiceTest
     {
+        /// <summary>
+        /// 一覧取得テスト
+        /// </summary>
+        [Fact]
+        public void GetToDoItems()
+        {
+            //repositoryのMockデータを生成
+            var repository = new Mock<IRepository<ToDoItem>>();
+
+            //ToDoServiceを生成
+            var todoService = new ToDoService(repository.Object);
+
+            //repositoruのGetAllの返り値をnullに設定
+            repository.Setup(x => x.GetAll()).Returns((List<ToDoItem>)null);
+
+            //取得処理を実行
+            var items = todoService.GetToDoItems();
+
+            //取得処理が呼ばれたか確認
+            repository.Verify(x => x.GetAll());
+
+            //取得結果がnullでない事を確認
+            Assert.NotEqual(items, null);
+        }
+
         /// <summary>
         /// 新規登録テスト
         /// </summary>

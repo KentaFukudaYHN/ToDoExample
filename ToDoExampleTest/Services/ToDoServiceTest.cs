e668ba198461f39dd2242381134c368d4963307b
KentaFukudaYHN <yhn18782@gmail.com>
2020-03-12 06:01:05 +0900

    詳細取得テスト実装

--- ToDoExampleTest/Services/ToDoServiceTest.cs
+++ ToDoExampleTest/Services/ToDoServiceTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Moq;
 using ToDoExample.Entities;
 using ToDoExample.Interfaces;
@@ -16,7 +17,29 @@ namespace ToDoExampleTest.Services
         /// 一覧取得テスト
         /// </summary>
         [Fact]
-        public void GetToDoItems()
+        public void GetItemList()
+        {
+            //repositoryのMockデータを生成
+            var repository = new Mock<IRepository<ToDoItem>>();
+
+            //ToDoServiceを生成
+            var todoService = new ToDoService(repository.Object);
+
+            //repositoruのGetAllの返り値を設定
+            repository.Setup(x => x.GetAll()).Returns(new List<ToDoItem>());
+
+            //取得処理を実行
+            todoService.GetItemList();
+
+            //取得処理が呼ばれたか確認
+            repository.Verify(x => x.GetAll());
+        }
+
+        /// <summary>
+        /// 一覧取得テスト/一覧がnullの場合
+        /// </summary>
+        [Fact]
+        public void GetItemListWithNull()
         {
             //repositoryのMockデータを生成
             var repository = new Mock<IRepository<ToDoItem>>();
@@ -35,6 +58,55 @@ namespace ToDoExampleTest.Services
 
             //取得結果がnullでない事を確認
             Assert.NotEqual(items, null);
+        }
+
+        /// <summary>
+        /// ToDo詳細テスト
+        /// </summary>
+        [Fact]
+        public void GetItem()
+        {
+            //repositoryのMockデータを生成
+            var repository = new Mock<IRepository<ToDoItem>>();
+
+            //ToDoServiceを生成
+            var todoService = new ToDoService(repository.Object);
+
+            //repositoryの取得処理の設定
+            var id = "testId";
+            repository.Setup(x => x.GetById(id)).Returns(new ToDoItem());
+
+            //取得処理
+            todoService.GetItem(id);
+
+            //取得処理が呼ばれたか確認
+            repository.Verify(x => x.GetById(id));
+        }
+
+        /// <summary>
+        /// ToDo詳細取得テスト/存在しないToDoのID
+        /// </summary>
+        [Fact]
+        public void GetItemWithNotExist()
+        {
+            //repositoryのMockデータを生成
+            var repository = new Mock<IRepository<ToDoItem>>();
+
+            //ToDoServiceを生成
+            var todoService = new ToDoService(repository.Object);
+
+            //repositoryのGetByIdの返り値をnullに設定
+            var id = "notExitId";
+            repository.Setup(x => x.GetById(id)).Returns((ToDoItem)null);
+
+            //取得処理
+            Action getbyIdAction = () => todoService.GetItem(id);
+
+            //ArgumentExceptionの確認
+            Assert.Throws<ArgumentException>(getbyIdAction);
+
+            //repositoryの取得処理が呼ばれたか確認
+            repository.Verify(x => x.GetById(id));
         }
 
         /// <summary>
